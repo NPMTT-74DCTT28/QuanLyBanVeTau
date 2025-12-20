c5e46c3650f503fdf331ddbb89b998457b003051
Hiamnaut <nguyenvantuan21102005@gmail.com>
2025-12-20 10:50:08 +0700

    hoàn thiện gheDAO.insert

--- src/main/java/com/group3tt28/quanlybanvetau/dao/GheDAO.java
+++ src/main/java/com/group3tt28/quanlybanvetau/dao/GheDAO.java
@@ -5,6 +5,10 @@
 package com.group3tt28.quanlybanvetau.dao;
 
 import com.group3tt28.quanlybanvetau.model.Ghe;
+import com.group3tt28.quanlybanvetau.util.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,9 +24,23 @@ public class GheDAO {
     private static final String COT_ID_TOA_TAU = "id_toa_tau";
     
     public boolean insert(Ghe ghe) {
-        //Viết code INSERT (đặt trong try-cacth)
-        //return ps.executeUpdate() > 0 (PreparedStatement)
-        return true;
+        if (ghe == null ){
+            return false;
+        }
+        
+        String sql = "INSERT INTO " + TEN_BANG + " ("
+                + COT_SO_GHE + "," + COT_ID_TOA_TAU 
+                + ") VALUES (?, ?)";
+        
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, ghe.getSoGhe());
+            ps.setInt(2, ghe.getIdToaTau());
+            
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException("Xay ra loi he thong khi them ghe: " + e.getMessage(), e);
+        }
+       
     }
     
     public boolean update(Ghe ghe) {
