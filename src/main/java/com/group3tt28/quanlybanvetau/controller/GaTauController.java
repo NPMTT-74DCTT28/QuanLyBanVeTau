3992ed1945b96a694cfd7adf7104638a70a6689b
Thanh-ops <nguyenphucthanh10112005@gmail.com>
2025-12-27 13:39:31 +0700

    cap nhat them ga tau

--- src/main/java/com/group3tt28/quanlybanvetau/controller/GaTauController.java
+++ src/main/java/com/group3tt28/quanlybanvetau/controller/GaTauController.java
@@ -1,5 +1,7 @@
 package com.group3tt28.quanlybanvetau.controller;
 
+import com.group3tt28.quanlybanvetau.dao.GaTauDAO;
+import com.group3tt28.quanlybanvetau.model.GaTau;
 import com.group3tt28.quanlybanvetau.view.GaTauPanel;
 
 import java.awt.event.ActionEvent;
@@ -16,7 +18,7 @@ public class GaTauController {
         view.setVisible(true);
     }
     public static class add_gatau implements ActionListener {
-
+        GaTauDAO dao = new GaTauDAO();
         @Override
         public void actionPerformed(ActionEvent e) {
             String maGa = view.getMaGa();
@@ -28,7 +30,25 @@ public class GaTauController {
                 view.showError("Cần điền đầy đủ thông tin!");
                 return;
             }
-            
+
+            if (dao.checkTrung(maGa)) {
+                view.showError("Mã ga đã tồn tại!");
+                return;
+            }
+
+            GaTau gaTau = new GaTau(maGa, tenGa, dc, tp);
+
+            boolean success = dao.insert(gaTau);
+            if (!success) {
+                view.showError("Thêm ga tàu thất bại!");
+                return;
+            }
+
+            view.addRow(new Object[]{
+                    maGa, tenGa, dc, tp
+            });
+
+            view.showMessage("Thêm ga tàu thành công");
         }
     }
 
